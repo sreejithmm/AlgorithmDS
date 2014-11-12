4b9f2a33f00e851ebfa22c8cf429b1a2d2f31e60
Sreejith M M <sreejith.mm@gmail.com>
2014-11-12 23:09:46 +0530

    graph.c is updated

--- AlgorithmandDatastructureStudy/graph/graph.c
+++ AlgorithmandDatastructureStudy/graph/graph.c
@@ -5,45 +5,81 @@
 graph *
 createGraph (int nodes)
 {
-	int i=0;
-	graph* gr = (graph*)malloc(sizeof(graph));
-	gr->num=nodes;
-	gr->ArrList = (graphArrList*)malloc(nodes*sizeof(graphArrList));
-	for(i=0;i<nodes;i++)
-	{
-		gr->ArrList[i].head=NULL;
-	}	
+  int i = 0;
+  graph *gr = (graph *) malloc (sizeof (graph));
+  gr->num = nodes;
+  gr->ArrList = (graphArrList *) malloc (nodes * sizeof (graphArrList));
+  for (i = 0; i < nodes; i++)
+    {
+      gr->ArrList[i].head = NULL;
+    }
+	return gr;
+}
+
+void
+addVertices (graph * gr, int src, int dest)
+{
+  graphArrnode *node = NULL;
+
+  if (gr == NULL)
+    {
+      return;
+    }
+
+  if (gr->num == 0)
+    {
+      return;
+    }
+  if (gr->ArrList == NULL)
+    {
+      gr->ArrList = (graphArrList *) malloc (gr->num * sizeof (graphArrList));
+    }
+
+  node = (graphArrnode *) malloc (sizeof (graphArrnode));
+  node->dest = dest;
+  node->next = gr->ArrList[src].head;
+  gr->ArrList[src].head = node;
+
+  node = (graphArrnode *) malloc (sizeof (graphArrnode));
+  node->dest = src;
+  node->next = gr->ArrList[dest].head;
+  gr->ArrList[dest].head = node;
+
+
 
 }
 
-void addVertices(graph* gr, int src, int dest)
+void
+printGraph (graph * gr)
 {
-	graphArrnode* node=NULL;
+  int i;
+  graphArrnode *temp;
 
-	if(gr==NULL)
+  if (gr == NULL)
+    {
+      return;
+    }
+  if (gr->num == 0)
+    {
+      printf ("Nothing to print\n");
+    }
+  if (gr->ArrList == NULL)
+    {
+      printf ("ERR..No adjacency list found\n");
+      return;
+    }
+  for (i = 0; i < gr->num; i++)
+    {
+      temp = gr->ArrList[i].head;
+      printf ("\n");
+      printf ("%d :", i);
+      while (temp != NULL)
 	{
-		return;
-	}
 
-	if(gr->num == 0)
-	{
-		return;
+	  printf ("%d ", temp->dest);
+	  temp = temp->next;
 	}
-	if(gr->ArrList == NULL)
-	{
-		gr->ArrList = (graphArrList*)malloc(gr->num*sizeof(graphArrList));
-	}
-
-	node = (graphArrnode*)malloc(sizeof(graphArrnode));
-	node->dest = dest;
-	node->next = gr->ArrList[src].head;
-	gr->ArrList[src].head= node;
-	
-   	node = (graphArrnode*)malloc(sizeof(graphArrnode));
-	node->dest = src;
-	node->next = gr->ArrList[dest].head;
-	gr->ArrList[dest].head= node;
-	
-
+    }
+  return;
 
 }
--- AlgorithmandDatastructureStudy/graph/test.c
+++ AlgorithmandDatastructureStudy/graph/test.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include "graph.h"
+
 int main()
 {
     /* test */
+	int i,j,k,l,m,n;
+	graph* gr=NULL;
+	while(1)
+	{
+		printf("\nTEST MENU\n");
+		printf("*********\n");
+		printf("1.Create and print a graph\n");
+		scanf("%d",&m);
+		switch(m)
+		{
+			case 1:
+				printf("Enter no of nodes\n");
+				scanf("%d",&k);
+				gr=(graph*)createGraph(k);
+				addVertices(gr,0,1);
+				addVertices(gr,0,4);
+				addVertices(gr,1,2);
+				addVertices(gr,1,3);
+				addVertices(gr,1,4);
+				addVertices(gr,2,3);
+				addVertices(gr,3,4);
+				printGraph(gr);
+				break;
+		}
+
+	}
+
+
     return 0;
 }
