0210995faf94fb83bcda47da30c96fb89bd9b7b8
Sreejith M M <sreejith.mm@gmail.com>
2014-12-07 11:29:42 +0530

    single source shortest path

--- AlgorithmandDatastructureStudy/graph/graph.c
+++ AlgorithmandDatastructureStudy/graph/graph.c
@@ -343,6 +343,91 @@ int isDoubleEdgedGraph(graph* gr, int origin)
 }
 
 
+int getParent(int index)
+{
+
+    return((index-1)/2);
+
+}
+int getleftChild(int index)
+{
+    return((2*index)+1);
+}
+int getRightChild(int index)
+{
+    return((2*index)+2);
+}
+
+int insert(int heap[],int num,int* size)
+{
+    int location = *size;
+    int parentnode=getParent(location);
+
+    while(location >0)
+    {
+        if(heap[parentnode] < num)
+        {
+    
+            heap[location] = num;
+            *size = *size + 1;
+            return 0;
+        }
+        heap[location]=heap[parentnode];
+        location = parentnode;
+        parentnode = getParent(location);
+    }
+    heap[0]=num;
+    *size = *size + 1;
+    return 0;
+}
+
+int getMin(int heap[])
+{
+    return heap[0];
+}
+
+int getMinValueIdx(int heap[],int lidx, int ridx)
+{
+    if(heap[lidx] > heap[ridx])
+    {
+        return ridx;
+    }
+    return lidx;
+}
+void swap(int *a, int *b)
+{
+    int t;
+    t = *a;
+    *a = *b;
+    *b = t;
+    return ;
+}
+
+void heapify(int heap[],int index,int size)
+{
+
+    int lidx = getleftChild(index);
+    int ridx = getRightChild(index);
+    int smallestidx;
+
+    while(lidx <size && ridx < size)
+    {
+        smallestidx = getMinValueIdx(heap,lidx,ridx);
+        if(heap[smallestidx] > heap[index])
+        {
+            return ;
+        }
+        swap(&heap[smallestidx],&heap[index]);
+        index = smallestidx;
+        lidx = getleftChild(index);
+        ridx = getRightChild(index);
+
+    }
+
+}
+/*original code */
+
+
 heap* createHeap(graph* gr, int origin,int* size)
 {
 	heap* h = (heap*)malloc(sizeof(heap));
