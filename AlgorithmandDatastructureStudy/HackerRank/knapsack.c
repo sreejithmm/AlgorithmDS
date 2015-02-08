7004589a130460c2230736468dfc391baafc4cdd
Sreejith M M <sreejith.mm@gmail.com>
2015-02-08 23:23:04 +0530

    hacckerrank knapsack

--- AlgorithmandDatastructureStudy/HackerRank/knapsack.c
+++ AlgorithmandDatastructureStudy/HackerRank/knapsack.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int knapsack(int arr[],int cap,int num)
+int max(int a, int b)
 {
+	if(a>b)
+		return a;
+	else
+		return b;
+}
+
+int knapsack(int arr[],int capa,int num)
+{
+	int i,j;
 	int** matr=(int**) malloc(sizeof(int*) * (num+1));
 
-	for(i=0;i<num;i++)
+	for(i=0;i<=num;i++)
 	{
-		matr[i] = (int*)malloc(sizeof(int)*(cap+1));
+		matr[i] = (int*)malloc(sizeof(int)*(capa+1));
 	}
 	for(i=0;i<=num;i++)
 	{
 		for(j=0;j<=capa;j++)
 		{
-			if(j==0 || k==0)
+			if(j==0 || i==0)
 			{
 				matr[i][j] = 0;
 			}
-			if(arr[i] <= capa)
+			else if(arr[i-1] <= capa)
 			{	
-				matr[i][j] = 
+				matr[i][j] = max(arr[i-1] + matr[i][capa-arr[i-1]],matr[i-1][capa]);
+			}
+			else if(arr[i-1] > capa)
+			{
+				matr[i][j] = matr[i-1][capa];
 			}
 
 		}
 	}
-	
+	return matr[num][capa];
 }
 
 int main()
 {
-	int nT, int cap, int num,i,j,k;
+	int nT,  cap,  num,i,j,k;
 	int arr[2000];
 	int result[10];
 
@@ -39,11 +53,16 @@ int main()
 		scanf("%d",&cap);
 		for(j=0;j<num;j++)
 		{
-			scanf("%d",&arr[i]);
+			scanf("%d",&arr[j]);
 		}
 
 		result[i]=knapsack(arr,cap,num);
 
 	}
+	for(i=0;i<nT;i++)
+	{
+		printf("%d ",result[i]);
+	}
+	printf("\n");
 }
 
