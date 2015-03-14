f292ccb497d97affea5c40c4aed571bed14ec58b
Sreejith M M <sreejith.mm@gmail.com>
2015-03-14 13:12:08 +0530

    string pointer arithmetic

--- AlgorithmandDatastructureStudy/strings/multipointer.c
+++ AlgorithmandDatastructureStudy/strings/multipointer.c
@@ -11,16 +11,36 @@ void print_pointer_arith(char * str)
 	return;
 }
 
+
+void print_pointer_arith_2(char **str2)
+{
+#if 0
+	printf("array of pointers\n");
+	printf("%s\n",str2);
+	printf("%p\n",str2);
+	printf("%p\n",str2+1);
+
+	return;
+#endif
+}
+
 int main()
 {
 
 	char *str="name";
-	char *str[10]={"name","namak","horrible"};
+	
+	char (*str2)[10]="myan";//"fdma","horrible","Mikeeerer",NULL,NULL,NULL,NULL,NULL,NULL};
+	char arr[10]="hello";
 
 	print_pointer_arith(str);
-	print_pointer_arith_2(str);
+	//print_pointer_arith_2(str2);
 
-
+	printf("%s\n",*str2);
+	printf("%p\n",(str2));
+	printf("%p\n",str2+1);
+	printf("%s\n",*(str2+1));
+	printf("%s\n",*(str2+2));
+	printf("%s\n",*(str2+3));
 
 	return 0;
 	
