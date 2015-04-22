8c81310b0fd7d7c55396998029a3bae6a329b703
Sreejith M M <sreejith.mm@gmail.com>
2015-04-22 23:12:16 +0530

    decode

---
+++ AlgorithmandDatastructureStudy/misc/decode.py
@@ -0,0 +1,22 @@
+import sys
+def print_chars(string,val):
+    for i in range(len(string)):
+        if(string[i] != " "):
+            base =ord(string[i])%97
+            diff = base - val
+            if (diff < 0):
+                num = 122+diff+1
+            else:
+                num = diff+97
+            sys.stdout.write(chr(num))
+        else:
+            sys.stdout.write(string[i])
+    print ""
+
+string="Dhfufydk fudjdff kfiffjdj djdud uxucj cjxd uckcjdu sudu xhssyz hzu xjcjd duxjcud uxudj xhxuxhx ducj hash u uxhxd jccjuc."
+
+
+print "Original string:",string
+print ""
+for val in range (1,27):
+    print_chars(string.lower(),val)
