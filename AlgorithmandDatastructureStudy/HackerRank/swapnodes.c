1383e9bdfe2e993389ab59925f924872bda3ef47
sreejithmm <sreejith.mm@gmail.com>
2015-05-19 11:44:03 +0530

    swapnodes algo hackerrank/datastructures

---
+++ AlgorithmandDatastructureStudy/HackerRank/swapnodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct  _Tree
+{
+	/* data */
+	int val
+	int level;
+	struct _Tree *left;
+	struct _Tree *right;
+}Tree;
+
+Tree* tree_create()
+{
+	int i,j;
+	int n;
+
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		
+
+	}
+
+}
+int main();
