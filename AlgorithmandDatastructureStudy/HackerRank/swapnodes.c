f733a300245361ed6bc1d88f4c0320433718479b
Sreejith M M <sreejith.mm@gmail.com>
2015-05-19 23:13:35 +0530

    swapnodes-hackerrank

--- AlgorithmandDatastructureStudy/HackerRank/swapnodes.c
+++ AlgorithmandDatastructureStudy/HackerRank/swapnodes.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <assert.h>
 
+
+
+#ifdef FILE_INPUT
+FILE *fd;
+void init_test_file()
+{
+	fd = fopen("test.txt","r");
+}
+void readTwoInt(int *a, int *b)
+{
+	fscanf(fd,"%d",a);
+	fscanf(fd,"%d",b);
+	return;
+}
+void readInt(int *a)
+{
+	fscanf(fd,"%d",a);
+	return;
+}
+#endif
+
+int max(int a , int b)
+{
+	int max;
+	max =(a>b)?a:b;
+	return max;
+}
 typedef struct  _Tree
 {
 	/* data */
-	int val
+	int val;
 	int level;
 	struct _Tree *left;
 	struct _Tree *right;
@@ -73,18 +103,18 @@ void enqueue(myqueue *q,Tree* dat)
 Tree* dequeue(myqueue *q)
 {
 
-    int temp;
+    Tree* temp;
     struct queue_node *node;
     if(q==NULL)
     {
 
         printf("Create Q first. Q is NULL\n");
-        return INT_MIN;
+        return NULL;
     }
     if(q->front == NULL)
     {
         printf("Queue is empty\n");
-        return INT_MIN;
+        return NULL;
     }
     node = q->front;
     temp = node->data;
@@ -103,7 +133,7 @@ Tree* dequeue(myqueue *q)
 }
 
 
-Tree* tree_create(int *maxlevel,*nodes)
+Tree* tree_create(int *maxlevel,int *nodes)
 {
 	int i,j;
 	int n;
@@ -114,36 +144,50 @@ Tree* tree_create(int *maxlevel,*nodes)
 	int T,k;
 	Tree* root=NULL;
 
+	*maxlevel=1;
+#ifdef FILE_INPUT
+	readInt(&n);
+#else
 	scanf("%d",&n);
+#endif
 	if(n>0)
 	{
 		q=createQueue();
 
-		node = create_tree_node(val,level);
-		level ++;
+		node = create_tree_node(1,level);
 		enqueue(q,node);
 		root=node;
 	}
-	/* Read nodes */	
-	for(i=1;i<n;i++)
+	/* Read nodes */
+	while(q->front != NULL)
 	{
-		scanf("%d %d",lval,rval);
-		node = dequeue(q);
+#ifdef FILE_INPUT
+		readTwoInt(&lval,&rval);
+#else
+		scanf("%d %d",&lval,&rval);
+#endif
+		if(q->front != NULL)
+			node = dequeue(q);
+		else
+			break;
 		if(lval != -1)
 		{
-			
-			node->left = create_tree_node(lval,level)
-			enqueue(q,node);
+
+			node->left = create_tree_node(lval,node->level+1);
+			enqueue(q,node->left);
+			*maxlevel = max(*maxlevel, node->level+1);
 		}
 		if (rval != -1)
 		{
-			node->right = create_tree_node(rval,level);
-			enqueue(rval,node);
+			node->right = create_tree_node(rval,node->level+1);
+			enqueue(q,node->right);
+			*maxlevel = max(*maxlevel, node->level+1);
 		}
-		level++;
+
+
 	}
-	printf("maxlevel=%d\n",level );
-	*maxlevel = level;
+//	printf("maxlevel=%d\n",*maxlevel);
+
 	*nodes = n;
 	return root;
 
@@ -152,10 +196,14 @@ void print_inorder_of_tree(Tree *root)
 {
     if(root==NULL)
         return;
-    inorderTraverseInt(root->left);
+    print_inorder_of_tree(root->left);
     printf("%d ",root->val);
-    inorderTraverseInt(root->right);
+    print_inorder_of_tree(root->right);
 
+}
+void call_dummy()
+{
+	return;
 }
 void rearrage_nodes(Tree* root, int k,int n, int height)
 {
@@ -166,40 +214,61 @@ void rearrage_nodes(Tree* root, int k,int n, int height)
 	enqueue(q,root);
 	Tree* temp;
 
-	for(i=1;(i<n && k < height);i++)
+	while (q->front != NULL)
 	{
-		node = dequeue(q);
-
+		if(q->front != NULL)
+			node = dequeue(q);
+		else
+			break;
 		if (node->level %k == 0)
 		{
+			if(node->level == 12)
+				call_dummy();
 			temp = node->left;
 			node->left = node->right;
 			node->right = temp;
 		}
+	#if 0
 		if(node->level > k)
 		{
 			j++;
-			k = j*k
+			k = j*k;
 			if(k >= height)
 				break;
 		}
-		
+	#endif
+		if(node->left)
+			enqueue(q,node->left);
+		if(node->right)
+			enqueue(q,node->right);
+
 	}
 }
 int main()
 {
 	Tree* tree;
-	int nT,int k;
+	int nT,k;
 	int maxlevel;
 	int noofnodes;
+#ifdef FILE_INPUT
+	init_test_file();
+#endif
 
 	tree = tree_create(&maxlevel,&noofnodes);
+#ifdef FILE_INPUT
+	readInt(&nT);
+#else
 	scanf("%d",&nT);
+#endif
 	while(nT)
 	{
+#ifdef FILE_INPUT
+	readInt(&k);
+#else
 		scanf("%d",&k);
+#endif
 		/* k is the level */
-		if(k >= maxlevel)
+		if(k > maxlevel)
 		{
 			continue;
 		}
@@ -208,6 +277,8 @@ int main()
 			rearrage_nodes(tree,k,noofnodes,maxlevel);
 		}
 		print_inorder_of_tree(tree);
+		printf("\n");
+		nT--;
 	}
 
 
