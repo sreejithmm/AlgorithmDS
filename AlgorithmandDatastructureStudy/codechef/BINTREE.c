cbce3800db28afa10d27f7263803b46cd33c6ad8
Sreejith M M <sreejith.mm@gmail.com>
2015-05-23 12:23:40 +0530

    codechef-bintre

--- AlgorithmandDatastructureStudy/codechef/BINTREE.c
+++ AlgorithmandDatastructureStudy/codechef/BINTREE.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 
-typedef unsinged long long ULL;
+typedef unsigned long long ULL;
 
+ULL getparent(ULL src)
+{
+  return (src/2);
+}
+
+ULL leftchildof(ULL indx)
+{
+  return (2*indx );
+}
+
+ULL rightchildof(ULL indx)
+{
+  return (2*indx + 1);
+}
+
+ULL getshortestpath(ULL src, ULL dest)
+{
+  ULL parent ;
+  ULL path = 1;
+  parent = getparent (src);
+  if(parent == dest)
+  {
+    return path;
+  }
+  parent = src;
+  while(parent != dest)
+  {
+
+    if (leftchildof(parent) < dest)
+    {
+
+      parent = parent * 2;
+
+    }
+    else if(rightchildof(parent) < dest)
+    {
+      parent = parent * 2 + 1;
+
+    }
+    else
+    {
+      parent = getparent(parent);
+
+    }
+    path++;
+  }
+  return path;
+}
 
 void getmaxmin(ULL a, ULL b, ULL *max, ULL *min)
 {
@@ -31,8 +79,8 @@ int main()
     scanf("%llu %llu",&i,&j);
     getmaxmin(i,j,&max,&min);
     spath = getshortestpath(max,min);
-    
-    n--
+    printf("%llu",spath);
+    n--;
   }
 
 
