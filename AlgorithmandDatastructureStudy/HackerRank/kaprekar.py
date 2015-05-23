19e49de8cdb4fe47eb523abf9370fe3baebeae0c
Sreejith M M <sreejith.mm@gmail.com>
2015-05-23 11:36:49 +0530

    codechef - bintree

--- AlgorithmandDatastructureStudy/HackerRank/kaprekar.py
+++ AlgorithmandDatastructureStudy/HackerRank/kaprekar.py
@@ -16,24 +16,17 @@ else:
     for i in range(num1,num2+1):
         sqr= i*i
         if (sqr == 1):
-#            sys.stdout.write("1")
             print 1,
         else:
             j = str(sqr)
             len2 = len(str(j))
             firsth,secondh = j[:int((len2/2))] , j[int((len2/2)):]
-
             if firsth=="":
                 firsth='0'
             if secondh=="":
                 secondh='0'
-#            print firsth,secondh
             sumd = int(firsth) + int(secondh)
-#            print "sum of digists of firsth=",sum_digits(int(firsth))
-#            print "sum of digists of secondh=",sum_digits(int(secondh))
-#            print "number, square and sum of split squares",i,sqr,sumd
             if(sumd == i):
-#                sys.stdout.write(str(i))
                 print i,
                 exists = 1
 
--- AlgorithmandDatastructureStudy/codechef/BINTREE.c
+++ AlgorithmandDatastructureStudy/codechef/BINTREE.c
@@ -2,15 +2,36 @@
 
 typedef unsinged long long ULL;
 
+
+void getmaxmin(ULL a, ULL b, ULL *max, ULL *min)
+{
+  if (a > b)
+  {
+    *max = a;
+    *min = b;
+  }
+  else
+  {
+    *max = b;
+    *max = a;
+  }
+  return ;
+
+}
 int main()
 {
   ULL n,i,j;
+  ULL max,min;
+  ULL spath;
 
   scanf("%llu",&n);
 
   while(n)
   {
-
+    scanf("%llu %llu",&i,&j);
+    getmaxmin(i,j,&max,&min);
+    spath = getshortestpath(max,min);
+    
     n--
   }
 
