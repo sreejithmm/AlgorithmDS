987b372544ca96fc481e53523d5f15aa0930b1a0
sreejithmm <sreejith.mm@gmail.com>
2015-05-29 17:12:35 +0530

    kalki

--- AlgorithmandDatastructureStudy/codechef/KALKI.c
+++ AlgorithmandDatastructureStudy/codechef/KALKI.c
@@ -1,13 +1,41 @@
 //http://www.codechef.com/problems/KALKI
 
 typedef unsigned long long ULL;
+typedef long long LL;
+
+
+mytree* makeNode(LL x, LL y)
+{
+	mytree* node = (mytree*)malloc(sizeof(mytree));
+
+	node->xcord = x;
+	node->ycord = y;
+	node->right = NULL;
+	node->left = NULL;
+
+	/*TODO*/
+	
+	return node;
+}
+
 int main()
 {
 
 	ULL nT;
+	ULL n;
+	LL x,y;
+	mytree * node;
+
 	scanf("%d",&nT);
 	while(nT)
 	{
+		scanf("%d",&n);
+		while(n)
+		{
+			scanf("%d %d",&x,&y);
+			node = makeNode();
+
+		}
 		
 	}
 }
