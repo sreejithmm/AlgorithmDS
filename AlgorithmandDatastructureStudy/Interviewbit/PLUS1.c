f38d83fc89020782a445895e987bc09bb4c5ea66
Sreejith M M <sreejith.mm@gmail.com>
2015-06-13 20:09:48 +0530

    binary to bst

---
+++ AlgorithmandDatastructureStudy/Interviewbit/PLUS1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * @input A : Integer array
+ * @input n1 : Integer array's ( A ) length
+ *
+ * @Output Integer array. You need to malloc memory for result array, and fill result's length in length_of_array
+ */
+int* plusOne(int* A, int n1, int *length_of_array) {
+
+        /*
+         * *length_of_array = n1 + 1; // length of result array
+         * int *result = (int *) malloc(*length_of_array * sizeof(int));
+         * // DO STUFF HERE. NOTE : length_of_array can be n1 OR n1 + 1.
+         * return result;
+         */
+}
