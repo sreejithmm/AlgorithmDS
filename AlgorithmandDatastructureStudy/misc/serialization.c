c58d6f16013e296742ec94f5d5494e150fc1bf95
sreejithmm <sreejith.mm@gmail.com>
2015-06-23 13:59:55 +0530

    serialization of structures

--- AlgorithmandDatastructureStudy/misc/serialization.c
+++ AlgorithmandDatastructureStudy/misc/serialization.c
@@ -3,22 +3,32 @@
 
 typedef struct _structure{
 
-	int val1;
+	int age;
 	char * name;
-	int val2;
+	int compartment;
 }packet;
 
 
 packet * create_packet(int * len)
 {
 	int len;
+	packet* pArray;
+	char name[21]={'\0'};
+	int age, compartment;
 
 	printf("Enter the number of database entries you want to make:");
 	scanf("%d",&len);
+	pArray = (packet*)malloc(sizeof(packet)*len);
 
-	for(i=0;i<n;i++)
+	for(i=0;i<len;i++)
 	{
-		
+			printf("%d->Enter age,compartment,name in the order\n",i+1);
+			scanf("%d%d%s",&age,&compartment,name);
+			pArray[0].age = age;
+			pArray[0].compartment = compartment;
+			pArray[0].name = (char*)malloc(sizeof(char)*(strlen(name)+1));
+			strcpy(pArray[0].name,name);
+
 	}
 }
 int main()
