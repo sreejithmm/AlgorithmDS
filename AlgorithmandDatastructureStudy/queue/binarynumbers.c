c8bd7ee8afad37c6cb6fd1981f524825b41382db
unknown <smohanan@SMOHANAN-5V2Z0.cisco.com>
2015-09-20 15:12:13 +0530

    queue warmup

--- AlgorithmandDatastructureStudy/queue/binarynumbers.c
+++ AlgorithmandDatastructureStudy/queue/binarynumbers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <assert.h>
 
 typedef struct node
 {
   int val;
   struct node* next;
-}
+}NODE;
 
 typedef struct _queue
 {
@@ -13,6 +14,73 @@ typedef struct _queue
 }queue;
 
 
+queue* createqueue()
+{
+  queue* myq;
+
+  myq = (queue*)malloc(sizeof(queue));
+  assert(myq);
+  myq->head = NULL;
+  myq->tail = NULL;
+
+  return myq;
+
+}
+
+int dequeue(queue* myq)
+{
+  queue* next;
+  int val;
+
+  assert(myq);
+
+  assert(myq->head);
+
+  next = myq->head;
+  val = myq->head->val;
+
+  if (myq->head->next == NULL)
+  {
+    myq->head = NULL;
+    myq->tail = NULL;
+  }
+
+  free(next);
+
+}
+
+
+void enqueue(queue* myq, int val)
+{
+  NODE* node;
+
+  assert(myq);
+  node = (NODE*)malloc(sizeof(NODE));
+  node->val = val;
+  node->next = NULL;
+
+  if(myq->head== NULL)
+  {
+    myq->head = node;
+    myq->tail = node;
+  } 
+  else
+  {
+    myq->tail->next = node;
+    myq->tail = node;
+  } 
+  return;
+
+}
+
+int isEmpty(queue* myq)
+{
+  if(myq->head == NULL)
+    return 1;
+  else
+    return 0;
+}
+
 void print_bins(int nums)
 {
   int val=0;
@@ -20,7 +88,7 @@ void print_bins(int nums)
   queue* q = createqueue();
 
   enqueue(q,1);
-  while(nums--)
+  while(nums-- && !isEmpty(q))
   {
       val = dequeue(q);
 
