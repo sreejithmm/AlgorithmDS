7a479a9d89708c81c18d5e3e449b8153b10bd84b
Sreejith <sreejith.mm@gmail.com>
2021-06-13 11:25:04 +0530

    fib2 one mistake

--- AlgorithmandDatastructureStudy/DP/practice/fib/app.py
+++ AlgorithmandDatastructureStudy/DP/practice/fib/app.py
@@ -1,28 +1,38 @@
 #https://www.codechef.com/UADPIP01/problems/FIBONUM
 import numpy as np
+import logging
+
+dp=[None]*1000
+dp[0]= 0
+dp[1] = 1
+
 def main():
     tests = int(input())
-    dp=[None]*1000
-    dp[0]= 0
-    dp[1] = 1
-    print(tests)
+    logging.basicConfig(filename='logger.log', encoding='utf-8', level=logging.DEBUG)
     while (tests):
         n = int(input())
-        print(n)
+        logging.warning('start of test:%d',n)
         val = fib(n)
         tests = tests-1
-    print(val)
+        print(val)
+    
 
         
 
-def fib(n):    if ( n==1):
+def fib(n):    
+    
+    logging.debug('fib entry = %d',n)  # will print a message to the console
+    total = 0
+    if ( n==1):
         return 1
     if (n==0):
         return 0
-    
+    if(dp[n] is not None):
+        return dp[n]
     if( n-2 >= 0):
             total =  (fib(n-1)+fib(n-2))
 
+    dp[n] = total
     return total
 
 if __name__ == "__main__":
