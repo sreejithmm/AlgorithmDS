eeea7439112433c20d8d50b7ceef6868007c2284
Sreejith <sreejith.mm@gmail.com>
2021-06-14 11:26:05 +0530

    mxad setup completed

--- AlgorithmandDatastructureStudy/DP/practice/MXAD/app.py
+++ AlgorithmandDatastructureStudy/DP/practice/MXAD/app.py
@@ -7,3 +7,23 @@ logging.basicConfig(filename='logger.log', encoding='utf-8', level=logging.DEBUG
 dp=[None]*1000
 dp[0]= 0
 dp[1] = 1
+
+def mxad(n):
+    pass
+
+def main():
+    tests = input()
+    tests = int(tests)
+    logging.basicConfig(filename='logger.log', encoding='utf-8', level=logging.DEBUG)
+    while (tests):
+        n = input()
+        n = int(n)
+        logging.warning('start of test:%d',n)
+        val = fib(n)
+        tests = tests-1
+        print(val)
+    return 0
+if __name__ =="__main__":
+    main()
+
+
